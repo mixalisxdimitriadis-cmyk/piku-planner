6a8ed43dbd27a68c46c2ebad96d867c6f3643ee7
mixalisxdimitriadis-cmyk <mixalisxdimitriadis@gmail.com>
2026-02-16 18:20:55 +0200

    Create Create tailwind.config.js

---
+++ styles/Create tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+4. Πάτα **"Commit changes"**
+
+---
+
+## Αρχείο 7: postcss.config.js
+
+1. Πάτα **"Add file"** → **"Create new file"**
+
+2. Όνομα:
+```
+postcss.config.js
